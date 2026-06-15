2d39e9c2490b5e3d141b7941cc248ed2e3ae0460
John Webster <j.webster@reply.com>
2026-06-15 16:39:08 -0700

    commit MVC

---
+++ CleanArch.UI.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.UI.Mvc.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
